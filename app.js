c8aa6057bc600fe0252d5017b6bf1d2518ab013b
mateoo04 <144594500+mateoo04@users.noreply.github.com>
2025-03-02 15:03:09 +0100

    Styled the page with Bootstrap

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ const port = process.env.PORT || 4000;
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
